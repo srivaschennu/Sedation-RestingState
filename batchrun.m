d4b63c0c09aeb83f9563ea3685acea6e9256c1cc
Srivas Chennu <srivas@gmail.com>
2014-09-30 23:00:26 +0000

    Checked in Wed Oct  1 00:00:26 BST 2014
    git-svn-id: file:///Users/chennu/Dropbox/Sources/Sedation-RestingState/trunk@9 66c5a5ed-8535-40a8-b9bd-6f10aed02a5b

--- batchrun.m
+++ batchrun.m
@@ -1,9 +1,21 @@
 function batchrun(listname)
 
 loadsubj
+loadpaths
+
 subjlist = eval(listname);
 
 for s = 1:size(subjlist,1)
     basename = subjlist{s,1};
-    calcpac(basename);
+    EEG = pop_loadset('filepath',filepath,'filename',[basename '.set'], 'loadmode','info');
+    batchres{s,1} = basename;
+    batchres{s,2} = subjlist{s,2};
+    batchres{s,3} = EEG.trials;% * EEG.pnts/EEG.srate;
+    if isfield(EEG,'rejepoch')
+        batchres{s,4} = length(EEG.rejepoch);% * EEG.pnts/EEG.srate;
+    else
+        batchres{s,4} = 0;
+    end
 end
+
+save batchres.mat batchres
